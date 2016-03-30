fea6132f756c474cac5b97b2a981c092889add33
孙健 <15998635127@163.com>
2016-03-30 20:25:12 +0800

    viewpager recyclerview

---
+++ app/src/main/java/com/example/dllo/mirror/adapterworks/HomeFragmentAdapter.java
@@ -0,0 +1,32 @@
+package com.example.dllo.mirror.adapterworks;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by dllo on 16/3/30.
+ */
+public class HomeFragmentAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mFragments;
+
+    public HomeFragmentAdapter(FragmentManager fm,List<Fragment> fragments) {
+        super(fm);
+        // TODO Auto-generated constructor stub
+        mFragments=fragments;
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return mFragments.get(arg0);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return mFragments.size();
+    }
+}
---
+++ app/src/main/java/com/example/dllo/mirror/adapterworks/ListFragmentAdapter.java
@@ -0,0 +1,85 @@
+package com.example.dllo.mirror.adapterworks;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.dllo.mirror.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dllo on 16/3/30.
+ */
+public class ListFragmentAdapter extends RecyclerView.Adapter<ListFragmentAdapter.ListViewHolder> {
+
+    private ArrayList<String> data;
+
+//    private MyItemListener listener;
+
+//    public void setMyItemListener(MyItemListener listener) {
+//        this.listener = listener;
+//    }
+
+    /**
+     * 自定义 添加数据方法
+     */
+    public void addData(ArrayList<String> data) {
+        this.data = data;
+        notifyDataSetChanged();  // 通知适配器  数据是实时更新的
+    }
+
+    // 缓存类
+    class ListViewHolder extends RecyclerView.ViewHolder  {
+
+        //        private TextView tv;
+        private ImageView iv;
+//        private int position;  // 用于监听事件
+//        private LinearLayout linearLayout;
+
+        //  缓存类 构造方法
+        public ListViewHolder(View itemView) {
+            super(itemView);
+            //  直接在这里  绑定组件
+
+            iv = (ImageView) itemView.findViewById(R.id.fragment_list_item_iv);
+
+        }
+
+    }
+
+
+    @Override
+    public ListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.fragment_list_item, null);
+        return new ListViewHolder(v);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(ListViewHolder holder, int position) {
+        if (data != null && data.size() > 0) {   // 判断  数据存在时
+
+        }
+    }
+
+
+    /**
+     * 返回数据个数
+     */
+    @Override
+    public int getItemCount() {
+        if (data != null && data.size() > 0) {
+            return data.size();
+        }
+        return 0;
+    }
+
+
+}
+
+
