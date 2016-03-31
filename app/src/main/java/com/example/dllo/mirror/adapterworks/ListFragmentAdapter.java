eba452c8bcc21f3d5a1c6a0daeef5a7b49b44b7e
孙健 <15998635127@163.com>
2016-03-31 11:33:05 +0800

    recyclerview为 眼镜分类时  的item

--- app/src/main/java/com/example/dllo/mirror/adapterworks/ListFragmentAdapter.java
+++ app/src/main/java/com/example/dllo/mirror/adapterworks/ListFragmentAdapter.java
@@ -54,7 +54,7 @@ public class  ListFragmentAdapter extends RecyclerView.Adapter<ListFragmentAdapt
     @Override
     public ListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).
-                inflate(R.layout.fragment_list_item, null);
+                inflate(R.layout.fragment_list_glassesitem, null);
         return new ListViewHolder(v);
     }
 
