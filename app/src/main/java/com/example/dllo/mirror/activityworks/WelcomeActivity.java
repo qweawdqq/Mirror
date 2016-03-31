188c6cd7f32b1f103a9ccab0e97ebacbef422b70
张桂华 <18840853816@163.com>
2016-03-31 20:43:30 +0800

    no message

--- app/src/main/java/com/example/dllo/mirror/activityworks/WelcomeActivity.java
+++ app/src/main/java/com/example/dllo/mirror/activityworks/WelcomeActivity.java
@@ -1,25 +1,78 @@
 package com.example.dllo.mirror.activityworks;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.dllo.mirror.R;
+import com.example.dllo.mirror.baseworks.BaseActivity;
+import com.example.dllo.mirror.bean.WelcomeBean;
+import com.example.dllo.mirror.net.NetListener;
+import com.example.dllo.mirror.net.OkHttpNetHelper;
+import com.example.dllo.mirror.normalstatic.StaticEntityInterface;
+import com.google.gson.Gson;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.Callback;
+
+import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * Created by dllo on 16/3/30.
  */
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends BaseActivity implements StaticEntityInterface{
 
-
-    private final long SPLASH_LENGTH = 1000;
+    private ImageView iv;
+    private final long SPLASH_LENGTH = 2000;
     Handler handler = new Handler();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                WelcomeBean bean = new Gson().fromJson(msg.obj.toString(),WelcomeBean.class);
+                Log.i("====",bean.getImg());
+                //显示图片的配置
+                DisplayImageOptions options = new DisplayImageOptions.Builder()
+                        .showImageOnLoading(R.mipmap.ic_launcher)   //加载过程中的图片
+                        .showImageOnFail(R.mipmap.ic_launcher) //加载失败的图片
+                        .cacheInMemory(true)//是否放到内存缓存中
+                        .cacheOnDisk(true)//是否放到硬盘缓存中
+                        .bitmapConfig(Bitmap.Config.RGB_565)//图片的类型
+                        .build();//创建
+
+                ImageLoader.getInstance().displayImage(bean.getImg(), iv, options);//参数1  是图片网址 ,参数2 是图片窗口 imgview,参数3 是上面创建的配置
+
+
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_welcome;
+    }
+
+    @Override
+    protected void initView() {
+        iv = bindView(R.id.activity_welcome_iv);
+
+    }
+
+    @Override
+    protected void initData() {
 
         // 使用handler的postDelayed实现延时跳转
         handler.postDelayed(new Runnable() {
@@ -29,7 +82,34 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, SPLASH_LENGTH); // 1秒后跳转至MainActivity
+        }, SPLASH_LENGTH); // 2秒后跳转至MainActivity
+
+        OkHttpUtils.post().url(STARTED_IMG).build().execute(new Callback() {
+            @Override
+            public Object parseNetworkResponse(Response response) throws Exception {
+                String body = response.body().string();
+                Log.d("----",body);
+                Message message = new Message();
+                message.obj = body;
+                handler.sendMessage(message);
+
+                return null;
+            }
+
+            @Override
+            public void onError(Call call, Exception e) {
+
+            }
+
+            @Override
+            public void onResponse(Object response) {
+
+            }
+        });
 
     }
+
+
+
+
 }
