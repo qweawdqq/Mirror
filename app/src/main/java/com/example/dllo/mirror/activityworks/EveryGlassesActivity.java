adfec1d9d27caeba1c3122c522db294947951f36
孙健 <15998635127@163.com>
2016-03-31 09:26:57 +0800

    测试

---
+++ app/src/main/java/com/example/dllo/mirror/activityworks/EveryGlassesActivity.java
@@ -0,0 +1,15 @@
+package com.example.dllo.mirror.activityworks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/3/31.
+ */
+public class EveryGlassesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
