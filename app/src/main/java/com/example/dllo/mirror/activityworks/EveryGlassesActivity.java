29cff79a1a8a7a6ae46f0db296e87fa57cf73373
孙健 <15998635127@163.com>
2016-03-31 09:38:39 +0800

    新建的activity

--- app/src/main/java/com/example/dllo/mirror/activityworks/EveryGlassesActivity.java
+++ app/src/main/java/com/example/dllo/mirror/activityworks/EveryGlassesActivity.java
@@ -3,13 +3,28 @@ package com.example.dllo.mirror.activityworks;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.dllo.mirror.R;
+import com.example.dllo.mirror.baseworks.BaseActivity;
+
 /**
  * Created by dllo on 16/3/31.
  */
-public class EveryGlassesActivity extends AppCompatActivity {
+public class EveryGlassesActivity extends BaseActivity {
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected int initLayout() {
+        return R.layout.activity_everyglasses;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
 
     }
 }
